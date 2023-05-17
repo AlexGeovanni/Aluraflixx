2fa4e26cc0dd90fdf3d16710e893bfc41432a64f
Alex Geovanni <A180065@unach.mx>
2023-05-17 02:14:45 -0600

    cambio de import

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 // import  jsonSever  from 'json-server';
-const jsonSever = require('json-server');
-const server = jsonSever.create();
-const router = jsonSever.router('./aluraflix.json');
-const middlewares = jsonSever.defaults();
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('aluraflix.json')
+const middlewares = jsonServer.defaults()
 
-server.use(middlewares);
-
-
-server.use(router);
-server.listen(4000,()=>{
-    console.log('JSON server is running');
+server.use(middlewares)
+server.use(router)
+server.listen(3000, () => {
+    console.log('JSON Server is running')
 })
